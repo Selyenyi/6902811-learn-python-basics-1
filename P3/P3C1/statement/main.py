a7259f6955bcc9ac658c06d723670414c7988e45
Selyenyi <111646794+Selyenyi@users.noreply.github.com>
2025-12-03 08:44:41 +0300

    Implement main function to calculate sum and product

--- P3/P3C1/statement/main.py
+++ P3/P3C1/statement/main.py
@@ -1,5 +1,9 @@
-# Write your code here!
-
+def main(): 
+    sum_result = operations.sum(3, 5)
+    print (sum_result)
+    
+    product_result = operations.product(8, 4)
+    print (product_result)# Write your code here!
 
 # Do not modify the code below
 if __name__ == "__main__":
